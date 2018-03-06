5884155344a0509641e4dc8561d9a7e774aa5121
zhouy546 <zhouyunqi546@gmail.com>
2018-03-06 17:32:59 +0800

    Initial commit

---
+++ Assets/MultiplayerGameProgramming/UI/ButtonClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Net;
+
+namespace HD
+{
+  public class ButtonClient : MonoBehaviour
+  {
+    public GameObject tcpClient, udp;
+
+    public void Awake()
+    {
+      GuiHelpers.OpenStep1();
+    }
+
+    public void OnClick()
+    {
+      Globals.isServer = false;
+
+      IPAddress ip = IPAddress.Parse(GameObject.Find("InputFieldIP").GetComponent<InputField>().text);
+
+     // udp.GetComponent<UDPChat>().serverIp = ip;
+      tcpClient.GetComponent<TCPChat>().serverIp = ip;
+
+   //   udp.SetActive(true);
+      tcpClient.SetActive(true);
+
+      GuiHelpers.OpenStep2();
+    }
+  }
+}
